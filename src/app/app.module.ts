7726446c233e42a043e31e1f6c43a26b363998e6
Ramiro Cosa <ramirocosa@gmail.com>
2022-10-29 12:54:57 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { HeaderComponent } from './commons/header/header.component';
+import { LeftBarComponent } from './commons/left-bar/left-bar.component';
+import { RightBarComponent } from './commons/right-bar/right-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BlogComponent,
+    HomeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    FooterComponent,
+    HeaderComponent,
+    LeftBarComponent,
+    RightBarComponent
   ],
   imports: [
     BrowserModule,
